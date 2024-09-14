d2cbbae301bf7842dbbaafb5913de6aa285e40ec
Ayush <ayushrgarg@gmail.com>
2024-09-14 10:08:32 -0400

    Initial Backend and Frontend setup

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+import cohere
+import numpy as np
+import os
+
+class Aura:
+    def __init__(self) -> None:
+        self.positive_aura = []
+        self.negative_aura = []
+
+    def load():
+        open("positive_aura.txt")
