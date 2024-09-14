41983aaf4646a1de75c199c5beffe2ee33f1f279
Ayush <ayushrgarg@gmail.com>
2024-09-14 16:24:31 -0400

    KMeans Clustering algorithm working, classification works

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@ import cohere
 import numpy as np
 import os
 from dotenv import load_dotenv
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin_min
 
 load_dotenv()
-
 
 class Aura:
     def __init__(self) -> None:
@@ -21,21 +22,36 @@ class Aura:
             self.negative_aura.append(line.strip())
 
     def get_positive_aura_embeddings(self):
-        return np.array(self.co.embed(texts=self.positive_aura, input_type='classification').embeddings[0])
+        embed = np.array(self.co.embed(texts=self.positive_aura, input_type='classification').embeddings)
+        return KMeans(n_clusters=10, n_init='auto').fit(embed)
 
     def get_negative_aura_embeddings(self):
-        return np.array(self.co.embed(texts=self.negative_aura, input_type='classification').embeddings[0])
+        embed = np.array(self.co.embed(texts=self.negative_aura, input_type='classification').embeddings)
+        return KMeans(n_clusters=10, n_init='auto').fit(embed)
     
     def embedding_comparison(self):
+        input_embed = np.array(self.co.embed(texts=["Aura game too strong"], input_type='classification').embeddings)
         pos_aura_embed = self.get_positive_aura_embeddings()
+        pos_aura_centroid = pos_aura_embed.cluster_centers_
+        pos_aura_closest, pos_aura_distance = pairwise_distances_argmin_min(input_embed, pos_aura_centroid)
+
         neg_aura_embed = self.get_negative_aura_embeddings()
-        input_embed = np.array(self.co.embed(texts=["Aura game too strong"], input_type='classification').embeddings[0])
+        neg_aura_centroid = neg_aura_embed.cluster_centers_
+        neg_aura_closest, neg_aura_distance = pairwise_distances_argmin_min(input_embed, neg_aura_centroid)
 
-        pos_cosine_similarity = np.dot(pos_aura_embed, input_embed) / (np.linalg.norm(pos_aura_embed) * np.linalg.norm(input_embed))
-        neg_cosine_similarity = np.dot(neg_aura_embed, input_embed) / (np.linalg.norm(neg_aura_embed) * np.linalg.norm(input_embed))
+        print(pos_aura_distance)
+        print(neg_aura_distance)
 
-        print(pos_cosine_similarity)
-        print(neg_cosine_similarity)
+        # Calculating the aura points assigned to each person
+        if pos_aura_distance > neg_aura_distance:
+            return self.aura_point_assignment(pos_aura_distance, neg_aura_distance)
+        elif neg_aura_distance < pos_aura_distance:
+            return self.aura_point_assignment(neg_aura_distance, pos_aura_distance)
+        else:
+            return 0
+
+    def aura_point_assignment(self, greater_distance, smaller_distance):
+        pass
 
 if __name__ == '__main__':
     aura = Aura()
